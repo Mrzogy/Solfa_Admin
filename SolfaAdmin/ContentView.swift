e012612acb73dfbccb1f450c0c2e0bf174929b52
mrzogy <magbolmrzogy@gmail.com>
2023-06-15 10:01:50 +0300

    Initial Commit

---
+++ SolfaAdmin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SolfaAdmin
+//
+//  Created by 3bood on 26/11/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SolfaAdmin/SolfaAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolfaAdminApp.swift
+//  SolfaAdmin
+//
+//  Created by 3bood on 26/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SolfaAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
